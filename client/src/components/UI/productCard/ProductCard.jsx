3102bf98714079fad01535a3f3954d401faa111f
Pavel Zinoviev <pzinoviev@happynumbers.com>
2023-09-11 23:50:54 +0300

    test: 1 2

--- client/src/components/UI/productCard/ProductCard.jsx
+++ client/src/components/UI/productCard/ProductCard.jsx
@@ -6,6 +6,7 @@ import cardImg2 from './img/card_flower/echeveria_2.jpeg';
 import cardImg3 from './img/card_flower/echeveria_3.jpeg';
 
 // добавить картинки каждому айтему в стор
+// test
 
 let currentItem;
 const images = [cardImg1, cardImg2, cardImg3];
